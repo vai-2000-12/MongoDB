1fd06a62be05734d286e0eeac2fb0ca6f1bf2c1f
Vaibhav soni <vaibhavsoni1712@gmail.com>
2024-09-12 23:00:42 +0530

    commit

--- MongoDB Tutorial/q.mongodb.js
+++ MongoDB Tutorial/q.mongodb.js
@@ -23,3 +23,34 @@ involve multiple fields or conditions that cannot be expressed using standard qu
 By using `$expr`, you can leverage the power of aggregation expressions to filter and manipulate
 data in a more flexible and efficient way. */
     
+use('FinancialData');
+db.sales.insertMany([
+    {volume : 100, target :120}, 
+    {volume : 89, target :80}, 
+    {volume : 200, target :177}
+]);
+db.sales.find().pretty();
+
+//to find documents where the volume amount exceeds the target:
+/* This MongoDB query is using the `$expr` operator along with the `$gt` (greater than) operator to
+find documents in the `sales` collection where the value of the `volume` field is greater than the
+value of the `target` field. The expression `{ : ["", ""] }` compares the values of
+the `volume` and `target` fields within the same document, returning documents where the `volume`
+exceeds the `target`. */
+db.sales.find({ $expr : {$gt: ["$volume", "$target"] } } ).pretty();
+
+//more complex Query:
+db.sales.find({
+    $expr: {
+      $gt: [
+        {
+          $cond: {
+            if: { $gte: ["$volume", 190] },
+            then: { $subtract: ["$volume", 10] },
+            else: "$volume"
+          }
+        },
+        "$target"
+      ]
+    }
+  }).pretty();
