56ee475e94fa0006b5ba4964eab995e6b5a51cbd
Vaibhav soni <vaibhavsoni1712@gmail.com>
2024-09-14 15:39:19 +0530

    commit

--- MongoDB Tutorial/v.mongodb.js
+++ MongoDB Tutorial/v.mongodb.js
@@ -3,4 +3,7 @@ use('user')
 db.users.find().pretty();
 
 //How to project the data of the arrays:-
-//db.movies.find({genere : "Drama"}, {"genere.$":1}).pretty();  
+//db.movies.find({genere : "Drama"}, {"genere.$":1}).pretty();
+
+// db.movird.find({gener : "Drama"}, {genere: {$elemMatch : {$eq : "Horror"}}});  
+
