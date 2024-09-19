f6412cd60e1ed351fd6ec6f4935a175d4d5e2ec2
Vaibhav soni <vaibhavsoni1712@gmail.com>
2024-09-19 18:25:23 +0530

    commit

---
+++ MongoDB Tutorial/Indexes in MongoDB/d.mongodb.js
@@ -0,0 +1,26 @@
+//Understanding the Partial Index :
+//Partial indexes is used with Conjection with Unique Index:-
+
+use('RandomData');
+db.user.insertMany([{name : "Max", email : "Max12@gmail.com"}, {name :"George"}])
+
+ //E11000 duplicate key error collection: test.user index: email_1 dup key: { email: null }
+db.user.insertOne({name : "Anna"}) 
+//Creating the Indexes :
+//Creating the Indexes As Unique..
+db.user.createIndex({email : 1});
+db.user.createIndex({email : 1}, {unique : true});
+db.user.dropIndex({email :1});
+
+db.user.createIndex({email :1}, {unique : true , partialFilterExpression : {email : {$exists : true}}});
+db.user.insertOne({name : "Anna"}) 
+db.user.find().pretty();
+db.user.getIndexes();
+
+//TTL Index:- Time To Live Index
+db.sessions.insertOne({data : "Dfasrae", createdAt : new Date()})
+db.sessions.find().pretty();
+// This expiredAfterSession is used only with the Date
+db.sessions.createIndex({createdAt : 1}, {expireAfterSeconds : 10});
+//Basically the Data Is being Deleted After 10 Seconds when I added this new Argument in the CreateIndex 
+db.sessions.find().pretty();
